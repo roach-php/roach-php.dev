1f93ac34cbe60c6dd369e2636215bcab762ee0e0
Kai Sassnowski <me@kai-sassnowski.com>
2021-12-26 22:02:25 +0100

    Add trademark emoji to page header

--- components/RoachLogo.vue
+++ components/RoachLogo.vue
@@ -1,3 +1,6 @@
 <template>
-  <span class="text-2xl font-bold tracking-tight text-gray-600">Roach</span>
+  <div class="inline-flex items-center space-x-2">
+    <span class="text-3xl">🐴</span>
+    <span class="text-2xl font-bold tracking-tight text-gray-600">Roach</span>
+  </div>
 </template>

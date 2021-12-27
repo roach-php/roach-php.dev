cb12aff8d677895ba640925626e4da871ded477f
Kai Sassnowski <me@kai-sassnowski.com>
2021-12-27 23:52:14 +0100

    Add Callout component

---
+++ components/Callout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-sky-100 px-4 py-4 border-l-4 border-sky-400">
+    <p class="font-semibold text-sky-900 !m-0">Note</p>
+
+    <div class="callout-content mt-2 text-sky-800">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.callout-content > *:first-child {
+  @apply mt-0;
+}
+
+.callout-content > *:last-child {
+  @apply mb-0;
+}
+</style>
